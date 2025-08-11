3d7585f9f6e5d80820c16703d15d57a83ffb5322
wlcrs <wlcrs@users.noreply.github.com>
2025-08-11 08:42:11 +0200

    Add integration test with tokio modbus server

---
+++ integration_tests/tokio/server.rs
@@ -0,0 +1,188 @@
+// SPDX-FileCopyrightText: Copyright (c) 2017-2025 slowtec GmbH <post@slowtec.de>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! # TCP server example
+//!
+//! This example shows how to start a server and implement basic register
+//! read/write operations.
+
+use std::{
+    collections::HashMap,
+    future,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use tokio::net::TcpListener;
+
+use tokio_modbus::{
+    prelude::{ Response, Request, ExceptionCode},
+    server::tcp::{accept_tcp_connection, Server},
+    server::rtu::Server as RtuServer,
+    server::rtu_over_tcp::Server as RtuOverTcpServer,
+    server::rtu_over_tcp::accept_tcp_connection as accept_rtu_over_tcp_connection
+};
+
+struct ExampleService {
+    input_registers: Arc<Mutex<HashMap<u16, u16>>>,
+    holding_registers: Arc<Mutex<HashMap<u16, u16>>>,
+}
+
+impl tokio_modbus::server::Service for ExampleService {
+    type Request = Request<'static>;
+    type Response = Response;
+    type Exception = ExceptionCode;
+    type Future = future::Ready<Result<Self::Response, Self::Exception>>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        let res = match req {
+            Request::ReadInputRegisters(addr, cnt) => {
+                register_read(&self.input_registers.lock().unwrap(), addr, cnt)
+                    .map(Response::ReadInputRegisters)
+            }
+            Request::ReadHoldingRegisters(addr, cnt) => {
+                register_read(&self.holding_registers.lock().unwrap(), addr, cnt)
+                    .map(Response::ReadHoldingRegisters)
+            }
+            Request::WriteMultipleRegisters(addr, values) => {
+                register_write(&mut self.holding_registers.lock().unwrap(), addr, &values)
+                    .map(|_| Response::WriteMultipleRegisters(addr, values.len() as u16))
+            }
+            Request::WriteSingleRegister(addr, value) => register_write(
+                &mut self.holding_registers.lock().unwrap(),
+                addr,
+                std::slice::from_ref(&value),
+            )
+            .map(|_| Response::WriteSingleRegister(addr, value)),
+            _ => {
+                println!("SERVER: Exception::IllegalFunction - Unimplemented function code in request: {req:?}");
+                Err(ExceptionCode::IllegalFunction)
+            }
+        };
+        future::ready(res)
+    }
+}
+
+impl ExampleService {
+    fn new() -> Self {
+        // Insert some test data as register values.
+        let mut input_registers = HashMap::new();
+        input_registers.insert(0, 1234);
+        input_registers.insert(1, 5678);
+        let mut holding_registers = HashMap::new();
+        holding_registers.insert(0, 10);
+        holding_registers.insert(1, 20);
+        holding_registers.insert(2, 30);
+        holding_registers.insert(3, 40);
+        Self {
+            input_registers: Arc::new(Mutex::new(input_registers)),
+            holding_registers: Arc::new(Mutex::new(holding_registers)),
+        }
+    }
+}
+
+/// Helper function implementing reading registers from a HashMap.
+fn register_read(
+    registers: &HashMap<u16, u16>,
+    addr: u16,
+    cnt: u16,
+) -> Result<Vec<u16>, ExceptionCode> {
+    let mut response_values = vec![0; cnt.into()];
+    for i in 0..cnt {
+        let reg_addr = addr + i;
+        if let Some(r) = registers.get(&reg_addr) {
+            response_values[i as usize] = *r;
+        } else {
+            println!("SERVER: Exception::IllegalDataAddress");
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+    }
+
+    Ok(response_values)
+}
+
+/// Write a holding register. Used by both the write single register
+/// and write multiple registers requests.
+fn register_write(
+    registers: &mut HashMap<u16, u16>,
+    addr: u16,
+    values: &[u16],
+) -> Result<(), ExceptionCode> {
+    for (i, value) in values.iter().enumerate() {
+        let reg_addr = addr + i as u16;
+        if let Some(r) = registers.get_mut(&reg_addr) {
+            *r = *value;
+        } else {
+            println!("SERVER: Exception::IllegalDataAddress");
+            return Err(ExceptionCode::IllegalDataAddress);
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let use_rtu = args.len() > 1;
+
+    if use_rtu {
+        let serial_port = &args[1];
+        let builder = tokio_serial::new(serial_port, 19200);
+        let server_serial = tokio_serial::SerialStream::open(&builder).unwrap();
+
+        println!("Starting up RTU server on {serial_port}...");
+        let _rtu_server = thread::spawn(move || {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let server = RtuServer::new(server_serial);
+            let service = ExampleService::new();
+            rt.block_on(async {
+                if let Err(err) = server.serve_forever(service).await {
+                    eprintln!("{err}");
+                }
+            });
+        });
+    }
+
+    
+
+    let rtu_socket_addr : SocketAddr = "127.0.0.1:5503".parse().unwrap();
+    let rtu_listener = TcpListener::bind(rtu_socket_addr).await?;
+    let _rtu_over_tcp_server = thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+
+        println!("Starting up server on {rtu_socket_addr}");
+        let server = RtuOverTcpServer::new(rtu_listener);
+        let new_service = |_socket_addr| Ok(Some(ExampleService::new()));
+        let on_connected = |stream, socket_addr| async move {
+            accept_rtu_over_tcp_connection(stream, socket_addr, new_service)
+        };
+        let on_process_error = |err| {
+            eprintln!("{err}");
+        };
+        rt.block_on(async {
+            if let Err(err) = server.serve(&on_connected, on_process_error).await {
+                eprintln!("{err}");
+            }
+        });
+    });
+
+
+    let socket_addr : SocketAddr = "127.0.0.1:5502".parse().unwrap();
+
+    println!("Starting up server on {socket_addr}");
+    let listener = TcpListener::bind(socket_addr).await?;
+    let server = Server::new(listener);
+    let new_service = |_socket_addr| Ok(Some(ExampleService::new()));
+    let on_connected = |stream, socket_addr| async move {
+        accept_tcp_connection(stream, socket_addr, new_service)
+    };
+    let on_process_error = |err| {
+        eprintln!("{err}");
+    };
+    server.serve(&on_connected, on_process_error).await?;
+    Ok(())
+}
+
+
---
+++ integration_tests/tokio/test_client.py
@@ -0,0 +1,92 @@
+"""Integration tests against the Rust Tokio Modbus server."""
+
+import subprocess
+import time
+from collections.abc import Generator
+from pathlib import Path
+
+import pytest
+
+from tmodbus.client import AsyncModbusClient
+from tmodbus.transport import AsyncRtuTransport, AsyncTcpTransport
+
+
+@pytest.fixture
+def log_traffic(caplog) -> None:
+    """Increase logging level for easy debugging."""
+    caplog.set_level("DEBUG", logger="tmodbus")
+
+
+@pytest.fixture(scope="session")
+def server() -> Generator[None]:
+    """Start socat and server process."""
+    # Use socat to create a virtual serial port
+    socat_process = subprocess.Popen(
+        [
+            "/usr/bin/socat",
+            "-d",
+            "-d",
+            "-v",
+            "pty,rawer,echo=0,link=./server-socket",
+            "pty,rawer,echo=0,link=./client-socket",
+        ],
+        cwd=str(Path(__file__).parent),
+    )
+
+    time.sleep(0.05)  # allow the socat process to start
+
+    # Start the server process and connect it to the socat server-socket
+    server_process = subprocess.Popen(  # noqa: S603
+        [
+            str(Path(__file__).parent / "target/release/server"),
+            str(Path(__file__).with_name("server-socket")),
+        ],
+    )
+
+    time.sleep(0.05)  # allow the server process to start
+
+    yield
+    server_process.kill()
+    socat_process.terminate()
+    server_process.wait()
+    socat_process.wait()
+
+
+@pytest.mark.parametrize(
+    "transport",
+    [
+        AsyncTcpTransport("127.0.0.1", 5502),
+        AsyncRtuTransport(str(Path(__file__).with_name("client-socket")), baudrate=19200),
+    ],
+    ids=["tcp", "rtu"],
+)
+async def test_client(log_traffic, server, transport):
+    client = AsyncModbusClient(transport=transport)
+    await client.connect()
+    # Perform read/write operations using the client
+
+    # First write to some registers
+    await client.write_multiple_registers(0, [10, 20, 30, 40], unit_id=1)
+
+    # now read the contents of the registers back
+    hr0_4 = await client.read_holding_registers(0, 4, unit_id=1)
+    assert hr0_4 == [10, 20, 30, 40]
+
+    # Write a single register
+    await client.write_single_register(0, 50, unit_id=1)
+    hr0_4 = await client.read_holding_registers(0, 4, unit_id=1)
+    assert hr0_4 == [50, 20, 30, 40]
+
+    # Read the input registers
+    ir0_1 = await client.read_input_registers(0, 2, unit_id=1)
+    assert ir0_1 == [1234, 5678]
+
+    await client.close()
+
+
+if __name__ == "__main__":
+    pytest.main(
+        [
+            str(Path(__file__).parent),
+        ]
+    )
