2a8e7d1e178e00bea124c9df728acf2e472f0aa4
wlcrs <wlcrs@users.noreply.github.com>
2026-07-14 09:31:20 +0200

    Refactor client integration tests to use make_virtual_serial_ports helper

--- integration_tests/simonvetter/compile.sh
+++ integration_tests/simonvetter/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go build -o client client.go
-go build -o server server.go
+docker run --rm -v "$(pwd)":/work  -w /work golang:1.22 go build -o client client.go
+docker run --rm -v "$(pwd)":/work  -w /work golang:1.22 go build -o server server.go
